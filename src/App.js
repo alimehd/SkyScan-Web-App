33146b8a29d7b381d83c23277870f57d6b6a726b
alimehd <ali.mehdi@mail.concordia.ca>
2022-07-08 12:01:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,7 @@ import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
 import "./App.css";
 
-const [selectedFile, setSelectedFile] = useState();
-const [isFilePicked, setIsFilePicked] = useState(false);
 
-const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsSelected(true);
-	};
-
-const handleSubmission = () => {
-		const formData = new FormData();
-
-		formData.append('File', selectedFile);
-
-		fetch(
-			'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5',
-			{
-				method: 'POST',
-				body: formData,
-			}
-		)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-	};
 
 const machine = {
   initial: "initial",
@@ -238,6 +211,36 @@ function App() {
   useEffect(() => {
     loadModel();
   }, [modelURL]); // Only re-run the effect if count changes
+
+  const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+
+	const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsSelected(true);
+	};
+
+	const handleSubmission = () => {
+		const formData = new FormData();
+
+		formData.append('File', selectedFile);
+
+		fetch(
+			'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log('Success:', result);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	};
+  
   
   return (
     <div>
