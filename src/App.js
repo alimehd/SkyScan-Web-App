fd4b0cf6a059d7ae8b1ac47df838dd38e4ab05e7
alimehd <ali.mehdi@mail.concordia.ca>
2022-07-08 12:18:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -196,6 +196,17 @@ function App() {
     complete: { action: reset, text: "Reset" }
   };
 
+  const Button = styled.button`
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+`;
+
   const FileUploader = props => {
     // Create a reference to the hidden file input element
     const hiddenFileInput = React.useRef(null);
